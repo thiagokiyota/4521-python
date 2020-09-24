4efe6a8f1e9659ff1025e93f4d75e8f997227575
Thiago Kiyota <thiago.kiyota@gmail.com>
2020-09-24 07:37:00 -0300

    Fix Flask model and included .gitignore file

--- HelpDesk/Models/Model.py
+++ HelpDesk/Models/Model.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask("run")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
